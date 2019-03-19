cbc7c804cececdf4e64405ccbc06152f020c7577
zhangming <zhangming@shopex.com>
2019-03-19 11:26:05 +0800

    文件目录

--- config.go
+++ config.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"encoding/json"
 	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
 type Config struct {
@@ -13,19 +15,26 @@ type Config struct {
 
 var Cfg *Config
 
-const config_file = "config.json"
+
+var configFile string
 
 func init() {
 	Cfg = &Config{}
-	buf, err := ioutil.ReadFile(config_file)
+	path, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+
+	configFile := path
+	configFile += "/config.json"
+
+	fmt.Printf("file path %s",configFile)
+	buf, err := ioutil.ReadFile(configFile)
 	if err != nil {
-		fmt.Printf("%s:%s\n", config_file, err)
+		fmt.Printf("%s:%s\n", configFile, err)
 	}
 
 	err = json.Unmarshal(buf, Cfg)
 
 	if err != nil {
-		fmt.Printf("error parsing %s:%s\n", config_file, err)
+		fmt.Printf("error parsing %s:%s\n", configFile, err)
 	}
 }
 
